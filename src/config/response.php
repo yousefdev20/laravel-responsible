213ebd2bbd3b25f927cfcbab3e5c2dbc843464fd
yousef <Y.abuanzeh@postaplus.com>
2021-12-22 14:35:22 +0200

    fixed the publish configuration

--- src/config/response.php
+++ src/config/response.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | PARAMETER_NAME_TO_LISTENER
+    |--------------------------------------------------------------------------
+    |
+    | To check on request have the parameter name coming from mobile to
+    | convert the response object to json otherwise the response
+    | will be web view
+    |
+    */
+
     "PARAMETER_NAME_TO_LISTENER" => env("PARAMETER_NAME_TO_LISTENER", 'from_app')
 ];
