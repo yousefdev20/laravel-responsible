4e3410b44e509b14ed325b0b01c7a50f2e24d3f5
yousef <Y.abuanzeh@postaplus.com>
2021-12-22 19:07:34 +0200

    fixed the publish configuration

--- src/config/response.php
+++ src/config/response.php
@@ -7,9 +7,9 @@ return [
     | PARAMETER_NAME_TO_LISTENER
     |--------------------------------------------------------------------------
     |
-    | To check on request have the parameter name coming from mobile to
-    | convert the response object to json otherwise the response
-    | will be web view
+    | To check on the header of the request have the parameter name coming
+    | from mobile to convert the response object to json otherwise
+    | the response will be web view
     |
     */
 
